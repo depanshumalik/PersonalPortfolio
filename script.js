44f3bd66555bf61e4beefc75c6ced320a7c979a7
depanshumalik <depanshumalik96@gmail.com>
2022-04-13 19:16:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const navButton = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navButton.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
+})
+
+navLinks.forEach( link => {
+    link.addEventListener('click', () => (document.body.classList.remove('nav-open')));
+})
+
